e067145ab482d5001065d8087c04af339491791d
Stephanie Cueto <stephaniem.cueto@gmail.com>
2020-02-13 19:00:55 -0800

    Updates

---
+++ precache-manifest.88c442684f0e0f6c10e50b1dca0dde81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "165df1ecb10227106abbea1e6e0029da",
+    "url": "/missAngie/static/media/avatar.165df1ec.jpeg"
+  },
+  {
+    "revision": "44ef2d3c0b11b4a3d61c",
+    "url": "/missAngie/static/js/runtime~main.44ef2d3c.js"
+  },
+  {
+    "revision": "c0d8a54b46bc619046a0",
+    "url": "/missAngie/static/js/main.c0d8a54b.chunk.js"
+  },
+  {
+    "revision": "c9dbe672637717e964c0",
+    "url": "/missAngie/static/js/1.c9dbe672.chunk.js"
+  },
+  {
+    "revision": "c0d8a54b46bc619046a0",
+    "url": "/missAngie/static/css/main.02c197f0.chunk.css"
+  },
+  {
+    "revision": "daa82c68650845e73c4ee81b979d0012",
+    "url": "/missAngie/index.html"
+  }
+];
